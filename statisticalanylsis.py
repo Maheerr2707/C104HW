6920f6a7efb9afc787af64ada8788798da3ba0e5
Maheerr2707 <62205613+Maheerr2707@users.noreply.github.com>
2021-05-30 17:58:38 +0530

    Add files via upload

---
+++ statisticalanylsis.py
@@ -0,0 +1,20 @@
+import csv
+with open("heightweight.csv",newline="") as f:
+    reader=csv.reader(f)
+    filedata=list(reader)
+
+#print(filedata)
+
+filedata.pop(0)
+newData=[]
+for i in range(len(filedata)):
+    n_num=filedata[i][2]
+    newData.append(float(n_num))
+
+n=len(newData)
+total=0
+for x in newData:
+    total+=x
+mean = total/n
+
+print("mean="+str(mean))
